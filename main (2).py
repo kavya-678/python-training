208b67b270249b463d90356b0f35b9ecd55fab32
kavya-678 <koramutlakavya@gmail.com>
2025-12-24 10:43:32 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x=int(input())
+for i in range (1, x+1):
+    
+        print("*"*i )
+
+    
