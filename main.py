668c7488a46d7cea918b40b72833e48aa5a8838d
kavya-678 <koramutlakavya@gmail.com>
2025-12-27 09:54:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,14 +5,11 @@
 Write your code in this editor and press "Run" button to execute it.
 
 '''
-
-x=int(input())
-for i in range (1,x+1):
-    if (i%3==0)and(i%5==0):
-        print ("fizz buzz")
-    elif i%5==0:
-            print ("buzz")
-    elif i%3==0:
-            print("fizz")
-    else:
-            print(i)
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or j==n-1 or i==j or j==n-i-1:
+            print("*",end="")
+        else:
+                print(" ",end="")
+    print()
